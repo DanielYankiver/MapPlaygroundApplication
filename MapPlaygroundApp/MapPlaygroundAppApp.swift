d2b7150776305095ac6d2c3b89823586ff62eb20
Daniel Yankiver <dy8950@att.com>
2024-05-23 14:14:54 -0400

    Initial Commit

---
+++ MapPlaygroundApp/MapPlaygroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPlaygroundAppApp.swift
+//  MapPlaygroundApp
+//
+//  Created by dy8950 on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MapPlaygroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
